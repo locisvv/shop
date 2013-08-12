1aca7a4afae52807b15dc1f712d01d5b2d9612f3
locisvv <locisvv@gmail.com>
2013-08-13 01:08:31 +0300

    configuration project

---
+++ main.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper' 
+
+DataMapper.setup(:default, 'sqlite:db.sqlite')
+
+Dir["./app/models/*.rb"].each { |file| require file }
+
+DataMapper.auto_migrate!
+
+# Dir["./app/helpers/*.rb"].each { |file| require file }
+Dir["./app/controllers/*.rb"].each { |file| require file }
+
