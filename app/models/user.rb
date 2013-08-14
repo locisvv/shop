2f7636f57720b17c3075cc83c8619ca72220eda6
locisvv <locisvv@gmail.com>
2013-08-14 17:32:04 +0300

    fixed model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,19 @@ class User
   	include DataMapper::Resource
 
 	property :id, Serial
-	property :firstname, String
-	property :lastname, String
-	property :email, String, 
-			 :length => (5..40),
-			 :unique => true,
-			 :format => :email_address,
+	property :firstname, String, :length => (3..40), :messages => {:length => @@errors[:length]}
+	property :lastname,  String, :length => (3..40), :messages => {:length => @@errors[:length]}
+	
+	property :email,     String, :length => (5..40), :unique => true, :format => :email_address,
 			 :messages => {
 		      	:is_unique => @@errors[:unique_email],
 		      	:format    => @@errors[:incorrect_email]
 			 }
-	property :login, String, 
-			 :unique => true,
+	property :login,     String, :length => (3..40), :unique => true,
 			 :messages => {
+			 	:length => @@errors[:length],
 			 	:is_unique => @@errors[:unique_login]
 			 }
 
-	property :password, String
-
-	# def presence_user
-	# 	found_user = self.first(:login => self.login, :password => self.password)
-	# 	found_user ? true : false
-	# end
-
-	#  validates_with_method :presence_user, :when => [:presence_user]
+	property :password,  String, :length => (3..40), :messages => {:length => @@errors[:length_password]}
 end
